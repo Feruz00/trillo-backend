2ee029e1a808f2a0e970516f4360acb489108433
Feruz00 <feruzatamyradow@mail.ru>
2022-11-04 20:56:34 +0500

    cors

--- app.js
+++ app.js
@@ -33,10 +33,12 @@ const User = require('./models/User');
 const Messages = require('./models/Message');
 const Follow = require('./models/Follow')
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors(
+//     {
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true
+// }
+));
 
 // app.use(helmet());
 
